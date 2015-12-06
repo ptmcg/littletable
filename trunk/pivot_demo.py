97854b583035b92835e57744091ed4b85580f385
ptmcg <ptmcg@467fc6f8-ff47-4562-b730-eda0dde076ec>
2015-12-06 04:25:26 +0000

    Cleanup/add pivot_demo.py to distribution

--- trunk/pivot_demo.py
+++ trunk/pivot_demo.py
@@ -14,12 +14,8 @@ from urllib import urlopen
 
 places = Table()
 
-# use one of these to read data either from my public Dropbox, or
-# download the data file locally and just read from the local file
-#~ data_source = urlopen('http://dl.dropbox.com/u/19965781/us_ppl.csv')
-data_source = 'US_PPL.CSV'
-
 # import from csv, convert elevation from meters to feet
+data_source = 'US_PPL.CSV'
 places.csv_import(data_source, 
                     transforms={'elev':lambda s:int(s)*33/10, 'pop':int})
 places.create_index('state')
