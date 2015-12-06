906d43d2bc366f83386336819d4a27be07b46b6e
Paul McGuire <ptmcg@austin.rr.com>
2015-12-06 00:18:08 +0000

    Add support for Python version-specific installers, plus wheel support

--- makeRelease.bat
+++ makeRelease.bat
@@ -1,4 +1,15 @@
 rmdir dist
 
 if exist MANIFEST del MANIFEST
-python setup.py sdist --formats=gztar,zip
+python setup.py sdist --formats=gztar,zip register
+
+if exist MANIFEST del MANIFEST
+
+python setup.py bdist_wheel
+python setup.py bdist_wininst --target-version=2.6 --plat-name=win32 upload register
+python setup.py bdist_wininst --target-version=2.7 --plat-name=win32 upload register
+python setup.py bdist_wininst --target-version=3.0 --plat-name=win32 upload register
+python setup.py bdist_wininst --target-version=3.1 --plat-name=win32 upload register
+python setup.py bdist_wininst --target-version=3.2 --plat-name=win32 upload register
+python setup.py bdist_wininst --target-version=3.3 --plat-name=win32 upload register
+python setup.py bdist_wininst --target-version=3.4 --plat-name=win32 upload register
