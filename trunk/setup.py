69e9ae6432d6dd207eb0bd4bf641b63c62275019
ptmcg <ptmcg@467fc6f8-ff47-4562-b730-eda0dde076ec>
2016-06-27 15:26:51 +0000

    Upgrade setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """Setup script for the littletable module distribution."""
-from distutils.core import setup
+from setuptools import setup
 
 import sys
 import os
@@ -27,5 +27,10 @@ setup(# Distribution meta-data
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        ]
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',        ]
     )
