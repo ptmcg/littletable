61429046df3e91ff7efcfd22722734fa4d404244
Paul McGuire <ptmcg@austin.rr.com>
2016-06-27 15:06:45 +0000

    Python 3 compatibility

--- littletable_demo.py
+++ littletable_demo.py
@@ -35,18 +35,18 @@ wishitems.insert(DataObject(custid="0030", sku="ROBOT-001"))
 wishitems.insert(DataObject(custid="0010", sku="ROBOT-001"))
 
 # print a particular customer name
-print customers.by.id["0030"].name
-print
+print(customers.by.id["0030"].name)
+print('')
 
 # print all items sold by the pound
 for item in catalog.where(unitofmeas="LB"):
-    print item.sku, item.descr
-print
+    print(item.sku, item.descr)
+print('')
 
 # print all items that cost more than 10
 for item in catalog.where(lambda ob : ob.unitprice>10):
-    print item.sku, item.descr, item.unitprice
-print
+    print(item.sku, item.descr, item.unitprice)
+print('')
 
 # join tables to create queryable wishlists collection - the following are all equivalent
 wishlists = (customers.join_on("id") + wishitems.join_on("custid")).join_on("sku") + catalog.join_on("sku")
@@ -54,33 +54,33 @@ wishlists = (customers.join_on("id") + wishitems.join_on("custid")).join_on("sku
 wishlists = catalog + (customers.join_on("id") + wishitems.join_on("custid")).join_on("sku")
 wishlists = catalog.join_on("sku") + (customers.join_on("id") + wishitems.join_on("custid"))
 wishlists = customers.join_on("id") + wishitems.join_on("custid") + catalog.join_on("sku")
-print wishlists().table_name
-print wishlists()("wishlists").table_name
+print(wishlists().table_name)
+print(wishlists()("wishlists").table_name)
 
 # print all wishlist items with price > 10
 bigticketitems = wishlists().where(lambda ob : ob.unitprice > 10)
 for bti in bigticketitems:
-    print bti
-print
+    print(bti)
+print('')
 
 # list all wishlist items by customer, then in descending order by unit price
 for item in wishlists().sort("custid, unitprice desc"):
-    print item
-print
+    print(item)
+print('')
 
 # create simple pivot table, grouping wishlist data by customer name
 wishlistsdata = wishlists()
 wishlistsdata.create_index("name")
 pivot = wishlistsdata.pivot("name")
 pivot.dump(row_fn=lambda o:"%s %s" % (o.sku,o.descr))
-print
+print('')
 
 # pivot on both sku number and customer name, giving tabular output
 piv2 = wishlistsdata.pivot("sku name")
 piv2.dump_counts()
-print
+print('')
 
 # pivot on both sku number and customer name, giving tabular output
 # tabulate by sum(unitprice) for all items in each pivot table cell
 piv2.dump_counts(count_fn=lambda recs:sum(r.unitprice for r in recs))
-print
+print('')
--- makeRelease.bat
+++ makeRelease.bat
@@ -1,15 +1,16 @@
 rmdir dist
 
 if exist MANIFEST del MANIFEST
-python setup.py sdist --formats=gztar,zip register
+python setup.py sdist --formats=gztar,zip upload 
+python setup.py register
 
 if exist MANIFEST del MANIFEST
 
 python setup.py bdist_wheel
 python setup.py bdist_wininst --target-version=2.6 --plat-name=win32 upload register
 python setup.py bdist_wininst --target-version=2.7 --plat-name=win32 upload register
-python setup.py bdist_wininst --target-version=3.0 --plat-name=win32 upload register
-python setup.py bdist_wininst --target-version=3.1 --plat-name=win32 upload register
-python setup.py bdist_wininst --target-version=3.2 --plat-name=win32 upload register
 python setup.py bdist_wininst --target-version=3.3 --plat-name=win32 upload register
 python setup.py bdist_wininst --target-version=3.4 --plat-name=win32 upload register
+python setup.py bdist_wininst --target-version=3.5 --plat-name=win32 upload register
+
+python setup.py upload_docs --upload-dir=htmldoc
