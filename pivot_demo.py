abc2c90a4fc15be6ecb9b44a3c6a630bf89869dd
Paul McGuire <ptmcg@austin.rr.com>
2016-06-27 15:49:30 +0000

    Linux-friendly filename case

--- pivot_demo.py
+++ pivot_demo.py
@@ -14,7 +14,7 @@ from littletable import Table
 places = Table()
 
 # import from csv, convert elevation from meters to feet
-data_source = 'US_PPL.CSV'
+data_source = 'us_ppl.csv'
 places.csv_import(data_source, 
                     transforms={'elev':lambda s:int(s)*33/10, 'pop':int})
 places.create_index('state')
