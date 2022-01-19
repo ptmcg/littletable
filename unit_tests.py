1f4ae0258bc87c1236aed8cecf8d9ebbf675142d
ptmcg <ptmcg@austin.rr.com>
2022-01-19 09:55:41 -0600

    Embed threaded http server code in unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -18,8 +18,6 @@ from typing import Optional, Union, NamedTuple
 
 import littletable as lt
 
-from test import csv_import_http_server
-
 try:
     import dataclasses
 except ImportError:
@@ -1617,10 +1615,53 @@ class TableImportExportTests:
             self.assertEqual(expected_type, tbl.import_source_type)
 
     def test_csv_import_from_url(self):
+        from http.server import HTTPServer, BaseHTTPRequestHandler
+        from http import HTTPStatus
+        import threading
+        import time
+        import urllib.error
         import urllib.request
 
+        class CSVTestRequestHandler(BaseHTTPRequestHandler):
+            def do_GET(self):
+                self.log_message("received %s %s", self.command, self.path)
+                path = self.path
+                if path == "/EXIT":
+                    self.send_response(HTTPStatus.OK)
+                    self.end_headers()
+                    threading.Thread(target=lambda: self.server.shutdown()).start()
+
+                elif path == "/":
+                    self.send_response(HTTPStatus.OK)
+                    self.end_headers()
+
+                elif path.startswith("/abc.csv"):
+                    send_bytes = b"a,b,c\n1,2,3\n"
+                    self.send_response(HTTPStatus.OK)
+                    self.end_headers()
+                    self.wfile.write(send_bytes)
+
+        def run(server_class=HTTPServer, handler_class=CSVTestRequestHandler):
+            server_address = ('', 8888)
+            httpd = server_class(server_address, handler_class)
+            httpd.serve_forever()
+
+        def run_background_test_server():
+            p = threading.Thread(target=run)
+            p.start()
+
+            for tries_remaining in reversed(range(20)):
+                try:
+                    with urllib.request.urlopen("http://localhost:8888/"):
+                        break
+                except urllib.error.URLError:
+                    if tries_remaining:
+                        time.sleep(0.25)
+
+            return p
+
         web_address = "http://localhost:8888"
-        p = csv_import_http_server.run_background_test_server()
+        p = run_background_test_server()
 
         url = web_address + "/abc.csv"
         try:
@@ -1628,7 +1669,6 @@ class TableImportExportTests:
         finally:
             with urllib.request.urlopen(web_address + "/EXIT"):
                 pass
-
             p.join()
 
         tbl.present()
