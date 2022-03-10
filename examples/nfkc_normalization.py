d2369c536bf5920f668b25987842e68c657bab29
ptmcg <ptmcg@austin.rr.com>
2022-03-10 10:16:09 -0600

    Some naming and code cleanup - add MIDDLE DOT to list of valid identifier chars (plus demonstration variable "_·")

--- examples/nfkc_normalization.py
+++ examples/nfkc_normalization.py
@@ -4,31 +4,41 @@
 #
 # Copyright Paul McGuire, 2022
 #
+import sys
 import unicodedata
 
 import littletable as lt
 
+ALL_UNICODE_CHARS = False
+
 # initialize accumulator for ASCII character to collect Unicode characters
-# that normalize back to ASCII
-accum = {ch: [] for ch in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_0123456789"}
+# that normalize back to ASCII characters that can be used in Python
+# identifiers (A-Z, a-z, 0-9, _, and ·)
+_· = "·"
+ident_chars = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+               "abcdefghijklmnopqrstuvwxyz"
+               "0123456789_" + _·)
+accum = {ch: [] for ch in ident_chars}
 
 # build up accumulator by walking Unicode range
-for i in range(32, 65536):  # sys.maxunicode+1):
+unicode_upper_limit = sys.maxunicode+1 if ALL_UNICODE_CHARS else 65536
+for i in range(32, unicode_upper_limit):
     ch = chr(i)
     norm = unicodedata.normalize("NFKC", ch)
     if norm in accum:
         accum[norm].append(ch)
 
 # convert accumulator to a littletable Table for presentation
-normalization = lt.Table()
-for a, norm in accum.items():
-    normalization.insert_many(
-        {"ASCII": a,
-         "ord": ord(a),
-         "Unicode": n,
-         "code_point": ord(n),
-         "name": unicodedata.name(n),
+normalizations = lt.Table()
+for asc_char, normalizing_chars in accum.items():
+    normalizations.insert_many(
+        {"ASCII": asc_char,
+         "ord": ord(asc_char),
+         "Unicode": norm_char,
+         "code_point": ord(norm_char),
+         "name": unicodedata.name(norm_char),
          }
-        for n in norm
+        for norm_char in normalizing_chars
     )
-normalization.present(groupby="ASCII ord")
+normalizations.sort("ASCII")
+normalizations.present(groupby="ASCII ord")
