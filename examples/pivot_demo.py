3866eea72cddc6d82140acd409d9bb164ce1dfac
ptmcg <ptmcg@austin.rr.com>
2024-12-03 10:05:00 -0600

    update pivot_demo.py and supporting data file

--- examples/pivot_demo.py
+++ examples/pivot_demo.py
@@ -7,55 +7,63 @@
 # Uses data excerpted from the US.TXT geographic names data file, provided
 # under Creative Commons Attribution 3.0 License by GeoNames (www.geonames.org)
 #
-# Copyright (c) 2011  Paul T. McGuire
+# Copyright (c) 2011, 2024  Paul T. McGuire
 #
 from littletable import Table
-import os
+from pathlib import Path
 
-this_dir, _ = os.path.split(__file__)
+
+this_dir = Path(__file__).parent
 
 # import from csv, convert elevation from meters to feet
 places = Table().csv_import(
-    os.path.join(this_dir, "us_ppl.zip"),
-    transforms={"elev": lambda s: int(s) * 33 / 10, "pop": int},
+    this_dir / "us_ppl.zip",
+    transforms={
+        "elev": lambda s: int(s) * 33 / 10,
+        "pop": int
+    },
 )
+print(places.info())
 
 # add computed field, elevation rounded down by 1000's
-places.compute_field("elev2", lambda x: int(x.elev / 1000) * 1000, 0)
+places.compute_field("elev_000", lambda x: int(x.elev / 1000) * 1000, 0)
 
-# create indexes
+# create indexes for use by pivots
 places.create_index("state")
-places.create_index("elev2")
+places.create_index("elev_000")
 
 print("summarize population by state")
 piv = places.pivot("state")
-pplByState = piv.as_table(sum, "pop").orderby("pop desc")
-for rec in pplByState:
+ppl_by_state = piv.as_table(sum, "pop").orderby("pop desc")
+for rec in ppl_by_state:
     print(rec.state, rec.pop)
+
+print()
 piv.dump_counts(count_fn=lambda recs: sum(r.pop for r in recs))
 
-print("")
+print()
 print("summarize population by elevation")
-piv = places.pivot("elev2")
-pplByElev = piv.as_table(sum, "pop")
-for rec in pplByElev:
-    print(rec.elev2, rec.pop)
-piv.dump_counts(count_fn=lambda recs: sum(r.pop for r in recs))
+piv = places.pivot("elev_000")
+ppl_by_elev = piv.as_table(sum, "pop")
+for rec in ppl_by_elev:
+    print(rec.elev_000, rec.pop)
 
-print("")
+print()
 print("summarize population by state and elevation")
-piv = places.pivot("state elev2")
-alaskan_locns_3k_to_4k_ft = piv["AK"][3000]
-for rec in alaskan_locns_3k_to_4k_ft:
-    print(rec)
-print("")
+piv = places.pivot("state elev_000")
 
+# dump all the sum of all population attributes for each row in each subtable
 piv.dump_counts(count_fn=lambda recs: sum(r.pop for r in recs))
-print("")
+print()
 
-pplByElev = piv.as_table(sum, "pop")
-for rec in pplByElev[:100]:
-    print(rec.state, rec.elev2, rec.pop)
+# select a subtable from the pivot table
+sd0 = piv["SD"][0]("SD places below 1000 ft elev")
+sd0.select("name pop elev elev_000").present()
+print()
+
+# pplByElev = piv.as_table(sum, "pop")
+# for rec in pplByElev[:100]:
+#     print(rec.state, rec.elev_000, rec.pop)
 
 # find average elevation of person by state
 print("Average elevation of each person by state")
